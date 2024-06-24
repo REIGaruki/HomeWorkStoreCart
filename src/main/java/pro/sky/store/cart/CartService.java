4eaee1cbc4a3017ab74c75160d6cf47d09c802ab
REIGaruki <gyakujo@mail.ru>
2024-06-24 13:51:24 +0300

    New Cart Interface

---
+++ src/main/java/pro/sky/store/cart/CartService.java
@@ -0,0 +1,13 @@
+package pro.sky.store.cart;
+
+import pro.sky.store.warehouse.Product;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public interface CartService {
+    ArrayList<Integer> addToCart(ArrayList<Integer> addedGoods);
+
+    HashSet<Product> viewCart();
+
+}
